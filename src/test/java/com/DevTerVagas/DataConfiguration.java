f91165a6f2bbf482857dcd1c1b47c00a18248c34
joyce moura <mileniorapper@gmail.com>
2024-08-05 22:04:45 -0300

    add: conf bd

--- src/test/java/com/DevTerVagas/DataConfiguration.java
+++ src/test/java/com/DevTerVagas/DataConfiguration.java
@@ -1,14 +1,13 @@
 package com.DevTerVagas;
 
 
-import jakarta.activation.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import jakarta.activation.DataSource;
 
-
+//configuraçaõ do banco de dados
 
 @Configuration
 public class DataConfiguration {
@@ -16,6 +15,11 @@ public class DataConfiguration {
     @Bean
     public DataSource dataSource (){
            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+           dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+           dataSource.setUrl("jdbc:mysql://localhost:3306/nome_do_banco");
+           dataSource.setUsername("root");
+           dataSource.setPassword("pti@2018");
+           return (DataSource) dataSource;
     }
 
 }
